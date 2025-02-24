ff1bcac948e6102aac44c68702e92ce0c6135c0f
connellr023 <connell.reffo@ucalgary.ca>
2025-02-24 14:48:00 -0700

    Redis connecting

--- cmd/tokenbase/tokenbase.go
+++ cmd/tokenbase/tokenbase.go
@@ -1,35 +1,46 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"net/http"
+	"tokenbase/internal/controllers"
 
+	"github.com/go-chi/chi/v5"
+	"github.com/redis/go-redis/v9"
 	"github.com/surrealdb/surrealdb.go"
 )
 
-const sdbDockerEndpoint = "ws://surrealdb:8000"
-const rdbDockerEndpoint = "redis:6379"
 const backendEndpoint = ":8090"
 
+// SurrealDB connection details
+const sdbDockerEndpoint = "ws://surrealdb:8000"
 const sdbNamespace = "tokenbaseNS"
 const sdbName = "tokenbaseDB"
+const sdbUsername = "root" // Hardcoded for now
+const sdbPassword = "root" // Hardcoded for now
 
-// Hardcode these for now
-const sdbUsername = "root"
-const sdbPassword = "root"
+// Redis connection details
+const rdbDockerEndpoint = "redis:6379"
+const rdbDatabase = 0
+const rdbPassword = "password" // Hardcoded for now
 
-func main() {
-	// Connect to SurrealDB over the Docker network
-	fmt.Printf("Connecting to SurrealDB at %s...\n", sdbDockerEndpoint)
-
+// Authenticate with SurrealDB
+//
+// Returns:
+// - A SurrealDB client
+// - A function to invalidate the authentication token
+// - An error if authentication fails
+func AuthSurrealDb() (*surrealdb.DB, func(), error) {
 	sdb, err := surrealdb.New(sdbDockerEndpoint)
 
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
 	// Set the namespace
 	if err := sdb.Use(sdbNamespace, sdbName); err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
 	// Authenticate
@@ -39,28 +50,86 @@ func main() {
 	})
 
 	if err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
 	// Check token validity
 	if err := sdb.Authenticate(token); err != nil {
-		panic(err)
+		return nil, nil, err
 	}
 
-	// Invalidate the token later
-	defer func(token string) {
+	// Allow caller to invalidateToken the token later
+	invalidateToken := func() {
 		if err := sdb.Invalidate(); err != nil {
 			panic(err)
 		}
-	}(token)
+	}
+
+	return sdb, invalidateToken, nil
+}
+
+// Authenticate with Redis
+//
+// Returns:
+// - A Redis client
+// - A default context to use with Redis commands
+// - An error if authentication fails
+func AuthRedis() (*redis.Client, context.Context, error) {
+	rdb := redis.NewClient(&redis.Options{
+		Addr:     rdbDockerEndpoint,
+		Password: rdbPassword,
+		DB:       rdbDatabase,
+	})
+
+	ctx := context.Background()
+	_, err := rdb.Ping(ctx).Result()
+
+	if err != nil {
+		return nil, ctx, err
+	}
+
+	return rdb, ctx, nil
+}
+
+func main() {
+	// Connect to SurrealDB over the Docker network
+	fmt.Printf("Connecting to SurrealDB at %s...\n", sdbDockerEndpoint)
+	sdb, invalidateToken, err := AuthSurrealDb()
+
+	if err != nil {
+		panic(err)
+	}
+
+	defer invalidateToken()
 
 	// Connect to Redis over the Docker network
 	fmt.Printf("Connecting to Redis at %s...\n", rdbDockerEndpoint)
+	client, ctx, err := AuthRedis()
 
-	// TODO ...
+	if err != nil {
+		panic(err)
+	}
+
+	// Create the controller injections
+	inj := controllers.Injection{
+		Sdb: sdb,
+		Rdb: controllers.RedisConnection{
+			Client: client,
+			Ctx:    ctx,
+		},
+	}
 
 	// Start the backend server
 	fmt.Printf("Starting backend server at %s...\n", backendEndpoint)
+	rootRouter := chi.NewRouter()
 
-	// TODO...
+	rootRouter.Route("/api", func(r chi.Router) {
+		r.Get("/hello", inj.GetHelloWorld)
+	})
+
+	if err := http.ListenAndServe(backendEndpoint, rootRouter); err != nil {
+		panic(err)
+	}
+
+	fmt.Printf("Backend server started at %s\n", backendEndpoint)
 }
--- go.mod
+++ go.mod
@@ -3,13 +3,43 @@ module tokenbase
 go 1.24.0
 
 require (
-	github.com/cespare/xxhash/v2 v2.2.0 // indirect
+	github.com/surrealdb/surrealdb.go v0.3.2
+	google.golang.org/api v0.222.0
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
 	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
-	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
 	github.com/go-chi/chi/v5 v5.2.1 // indirect
+)
+
+require (
+	cloud.google.com/go/auth v0.14.1 // indirect
+	cloud.google.com/go/auth/oauth2adapt v0.2.7 // indirect
+	cloud.google.com/go/compute/metadata v0.6.0 // indirect
+	github.com/felixge/httpsnoop v1.0.4 // indirect
+	github.com/fxamacker/cbor/v2 v2.7.0 // indirect
+	github.com/go-logr/logr v1.4.2 // indirect
+	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gofrs/uuid v4.4.0+incompatible // indirect
+	github.com/google/s2a-go v0.1.9 // indirect
+	github.com/google/uuid v1.6.0 // indirect
+	github.com/googleapis/enterprise-certificate-proxy v0.3.4 // indirect
+	github.com/googleapis/gax-go/v2 v2.14.1 // indirect
 	github.com/gorilla/websocket v1.5.3 // indirect
-	github.com/redis/go-redis/v9 v9.7.1 // indirect
-	github.com/surrealdb/surrealdb.go v0.3.2 // indirect
+	github.com/redis/go-redis/v9 v9.7.1
 	github.com/x448/float16 v0.8.4 // indirect
+	go.opentelemetry.io/auto/sdk v1.1.0 // indirect
+	go.opentelemetry.io/contrib/instrumentation/net/http/otelhttp v0.58.0 // indirect
+	go.opentelemetry.io/otel v1.34.0 // indirect
+	go.opentelemetry.io/otel/metric v1.34.0 // indirect
+	go.opentelemetry.io/otel/trace v1.34.0 // indirect
+	golang.org/x/crypto v0.33.0 // indirect
+	golang.org/x/net v0.35.0 // indirect
+	golang.org/x/oauth2 v0.26.0 // indirect
+	golang.org/x/sys v0.30.0 // indirect
+	golang.org/x/text v0.22.0 // indirect
+	google.golang.org/genproto/googleapis/rpc v0.0.0-20250212204824-5a70512c5d8b // indirect
+	google.golang.org/grpc v1.70.0 // indirect
+	google.golang.org/protobuf v1.36.5 // indirect
 )
