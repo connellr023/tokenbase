a55b1ac55dc010007c7708392df75151793cd91e
connellr023 <connell.reffo@ucalgary.ca>
2025-03-13 15:18:18 -0600

    Added color to icon button

--- web/src/components/IconButton.tsx
+++ web/src/components/IconButton.tsx
@@ -2,19 +2,38 @@ import styles from "@/styles/components/IconButton.module.scss";
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
+export enum IconButtonColor {
+  Red,
+}
+
 type IconButtonProps = {
   icon: IconProp;
   className?: string;
+  color?: IconButtonColor;
   onClick: () => void;
 };
 
 const IconButton: React.FC<IconButtonProps> = ({
   icon,
   className,
+  color,
   onClick,
 }) => {
+  let colorClassName = "";
+
+  if (color !== undefined) {
+    switch (color) {
+      case IconButtonColor.Red:
+        colorClassName = styles.red;
+        break;
+    }
+  }
+
   return (
-    <button className={`${styles.iconButton} ${className}`} onClick={onClick}>
+    <button
+      className={`${styles.iconButton} ${colorClassName} ${className}`}
+      onClick={onClick}
+    >
       <FontAwesomeIcon icon={icon} />
     </button>
   );
