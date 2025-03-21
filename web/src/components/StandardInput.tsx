3ef3ece4ba5380423665433eb7604eceef9d4499
NDMcCa <nicholas.m19@gmail.com>
2025-03-21 16:49:52 -0600

    work's functionally but is not applying the "invalid" styling in the desired way

--- web/src/components/StandardInput.tsx
+++ web/src/components/StandardInput.tsx
@@ -1,18 +1,36 @@
 import styles from "@/styles/components/StandardInput.module.scss";
-import React from "react";
+import React, { useState, useEffect } from "react";
 import { merriweather500 } from "@/utils/fonts";
 
 type StandardInputProps = {
   placeholder: string;
   type?: React.HTMLInputTypeAttribute;
   value?: string;
-  onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  isValid?: () => boolean;
+  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
 };
 
-const StandardInput: React.FC<StandardInputProps> = ({ placeholder, type, value, onChange }) => {
+
+const StandardInput: React.FC<StandardInputProps> = ({
+  placeholder,
+  type,
+  value,
+  isValid,
+  onChange,
+}) => {
+  const [hasError, setError] = useState(false);
+
+  useEffect(() => {
+    if (isValid) {
+      setError(!isValid());
+    }
+  }, [value, isValid]);
+
   return (
     <input
-      className={`${styles.main} ${merriweather500.className}`}
+      className={`${styles.main} ${merriweather500.className} ${
+        hasError ? styles.error : ""
+      }`}
       type={type}
       placeholder={placeholder}
       value={value}
--- web/src/pages/register/index.tsx
+++ web/src/pages/register/index.tsx
@@ -12,15 +12,15 @@ const Register: React.FC = () => {
   const steps = [
     <div key="step1">
       <p>Enter a username below.</p>
-      <StandardInput type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
+      <StandardInput type="text" placeholder="Username" value={username} isValid={() => validateStep(0)} onChange={(e) => setUsername(e.target.value)} />
     </div>,
     <div key="step2">
       <p>Enter an email below.</p>
-      <StandardInput type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
+      <StandardInput type="email" placeholder="Email" value={email} isValid={() => validateStep(1)} onChange={(e) => setEmail(e.target.value)} />
     </div>,
     <div key="step3">
       <p>Enter a password below.</p>
-      <StandardInput type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
+      <StandardInput type="password" placeholder="Password" value={password} isValid={() => validateStep(2)} onChange={(e) => setPassword(e.target.value)} />
     </div>,
   ];
 
@@ -39,7 +39,7 @@ const Register: React.FC = () => {
 
   const handleSubmit = () => {
     // Handle form submission
-    
+  
     const registerRequest: RegisterRequest = {
       username,
       email,
