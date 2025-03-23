e68627a77285d630d938a112568b6f68cb8bbf97
connellr023 <connell.reffo@ucalgary.ca>
2025-03-23 13:39:45 -0600

    Added UI for conversation records

--- web/src/components/RightDrawer.tsx
+++ web/src/components/RightDrawer.tsx
@@ -6,8 +6,9 @@ import { UserVariant } from "@/models/User";
 import { useRouter } from "next/router";
 import { useRightDrawerContext } from "@/contexts/RightDrawerContext";
 import { useBearerContext } from "@/contexts/BearerContext";
+import { useConversationRecordsContext } from "@/contexts/ConversationRecordsContext";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { firaMono400 } from "@/utils/fonts";
+import { firaMono400, merriweather400 } from "@/utils/fonts";
 import {
   faAnglesRight,
   faBolt,
@@ -22,6 +23,7 @@ const RightDrawer: React.FC = () => {
   const { push } = useRouter();
   const { isDrawerOpen, closeDrawer } = useRightDrawerContext();
   const { bearer, clearBearer } = useBearerContext();
+  const { conversationRecords } = useConversationRecordsContext();
 
   const pushAndClose = async (path: Url) => {
     await push(path);
@@ -53,67 +55,87 @@ const RightDrawer: React.FC = () => {
           </b>
         </div>
 
-        {/* Render message for guests */}
-        {!bearer || bearer.variant === UserVariant.Guest ? (
-          <div className={styles.noChatHistoryContainer}>
-            <div>
-              <FontAwesomeIcon icon={faShieldAlt} size="3x" />
-            </div>
-            <p>
-              Please <b>login</b> or <b>register</b> for the ability to manage
-              multiple conversations.
-            </p>
-          </div>
-        ) : (
-          <div className={styles.noChatHistoryContainer}>
-            <div>
-              <FontAwesomeIcon icon={faBoxOpen} size="3x" />
-            </div>
-            <p>
-              You have no conversation history. New conversations will appear
-              here once you start chatting.
-            </p>
-          </div>
-        )}
-
-        {/* Render footer area */}
-        <div className={styles.containerFooter}>
-          {/* Render login button for guests */}
+        <div>
+          {/* Render message for guests */}
           {!bearer || bearer.variant === UserVariant.Guest ? (
-            <>
-              <StandardButton
-                icon={faSignIn}
-                onClick={() => pushAndClose("/login")}
-              >
-                Login
-              </StandardButton>
-              <StandardButton
-                icon={faBolt}
-                onClick={() => pushAndClose("/register")}
-              >
-                Register
-              </StandardButton>
-            </>
+            <div className={styles.noConversationHistoryContainer}>
+              <div>
+                <FontAwesomeIcon icon={faShieldAlt} size="3x" />
+              </div>
+              <p>
+                Please <b>login</b> or <b>register</b> for the ability to manage
+                multiple conversations.
+              </p>
+            </div>
           ) : (
             <>
-              <div className={styles.profileContainer}>
-                <span
-                  className={`${styles.profileIcon} ${firaMono400.className}`}
-                >
-                  {(bearer.data?.username ?? "?")[0].toUpperCase()}
-                </span>
-                <span className={styles.email}>
-                  {bearer.data?.email ?? "None"}
-                </span>
-              </div>
-              <StandardButton icon={faPlus} onClick={() => {} /* TODO */}>
-                New Conversation
-              </StandardButton>
-              <StandardButton icon={faSignOut} onClick={logout}>
-                Logout
-              </StandardButton>
+              {conversationRecords.length === 0 ? (
+                <div className={styles.noConversationHistoryContainer}>
+                  <div>
+                    <FontAwesomeIcon icon={faBoxOpen} size="3x" />
+                  </div>
+                  <p>
+                    You have no conversation history. New conversations will
+                    appear here once you start chatting.
+                  </p>
+                </div>
+              ) : (
+                <ul className={styles.conversationHistoryContainer}>
+                  {conversationRecords.map((record, i) => (
+                    <li key={i}>
+                      <button
+                        className={merriweather400.className}
+                        onClick={() => {} /* TODO */}
+                        key={i}
+                      >
+                        {record.title}
+                      </button>
+                    </li>
+                  ))}
+                </ul>
+              )}
             </>
           )}
+
+          {/* Render footer area */}
+          <div className={styles.containerFooter}>
+            {/* Render login button for guests */}
+            {!bearer || bearer.variant === UserVariant.Guest ? (
+              <>
+                <StandardButton
+                  icon={faSignIn}
+                  onClick={() => pushAndClose("/login")}
+                >
+                  Login
+                </StandardButton>
+                <StandardButton
+                  icon={faBolt}
+                  onClick={() => pushAndClose("/register")}
+                >
+                  Register
+                </StandardButton>
+              </>
+            ) : (
+              <>
+                <div className={styles.profileContainer}>
+                  <span
+                    className={`${styles.profileIcon} ${firaMono400.className}`}
+                  >
+                    {(bearer.data?.username ?? "?")[0].toUpperCase()}
+                  </span>
+                  <span className={styles.email}>
+                    {bearer.data?.email ?? "None"}
+                  </span>
+                </div>
+                <StandardButton icon={faPlus} onClick={() => {} /* TODO */}>
+                  New Conversation
+                </StandardButton>
+                <StandardButton icon={faSignOut} onClick={logout}>
+                  Logout
+                </StandardButton>
+              </>
+            )}
+          </div>
         </div>
       </div>
     </>
