b79d07c322a9c3edd4037b95079faa8f017ced91
connellr023 <connell.reffo@ucalgary.ca>
2025-03-26 08:55:36 -0600

    Minor edits for frontend and added filter string util to backend

--- web/src/contexts/ConversationRecordsContext.tsx
+++ web/src/contexts/ConversationRecordsContext.tsx
@@ -1,22 +1,19 @@
 import Conversation from "@/models/Conversation";
-import { reqAllConversations } from "@/utils/reqAllConversations";
 import {
   createContext,
   Dispatch,
   ReactNode,
   SetStateAction,
   useContext,
-  useEffect,
   useState,
 } from "react";
-import { useBearerContext } from "./BearerContext";
 
 type ConversationRecordsContextType = {
   selectedConversationIndex: number | null;
   selectConversation: (index: number) => void;
   unselectConversation: () => void;
   conversationRecords: Readonly<Conversation[]> | null;
-  setConversationRecords: (conversations: Conversation[]) => void;
+  setConversationRecords: Dispatch<SetStateAction<Conversation[] | null>>;
   clearConversationRecords: () => void;
 };
 
@@ -26,36 +23,19 @@ const ConversationRecordsContext =
 export const ConversationRecordsProvider: React.FC<{ children: ReactNode }> = ({
   children,
 }) => {
-  const { bearer } = useBearerContext();
   const [selectedConversationIndex, setSelectedConversationIndex] = useState<
     number | null
   >(null);
-  const [conversationRecords, setConversationRecordsState] = useState<
+  const [conversationRecords, setConversationRecords] = useState<
     Conversation[] | null
   >([]);
 
-  const clearConversationRecords = () => setConversationRecordsState([]);
-
-  const setConversationRecords = (conversations: Conversation[]) =>
-    setConversationRecordsState(conversations);
+  const clearConversationRecords = () => setConversationRecords([]);
 
   const selectConversation = (index: number) =>
     setSelectedConversationIndex(index);
 
   const unselectConversation = () => setSelectedConversationIndex(null);
-
-  useEffect(() => {
-    if (!bearer) {
-      return;
-    }
-
-    const getAllConversations = async () => {
-      const conversations = await reqAllConversations(bearer.token);
-      setConversationRecordsState(conversations);
-    };
-
-    getAllConversations();
-  }, [bearer]);
 
   return (
     <ConversationRecordsContext.Provider
@@ -78,7 +58,7 @@ export const useConversationRecordsContext = () => {
 
   if (!context) {
     throw new Error(
-      "useConversationRecords must be used within a ConversationRecordsProvider",
+      "useConversationRecords must be used within a ConversationRecordsProvider"
     );
   }
 
