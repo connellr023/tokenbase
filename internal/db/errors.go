573f5c8f35429dae2fdb9e67736e00af1fdc87b6
NDMcCa <nicholas.m19@gmail.com>
2025-03-25 19:16:05 -0600

    Init branch

--- internal/db/errors.go
+++ internal/db/errors.go
@@ -3,6 +3,8 @@ package db
 import "errors"
 
 var (
-	ErrNoResults   = errors.New("no results found")
-	ErrQueryFailed = errors.New("query failed")
+	ErrNoResults     = errors.New("no results found")
+	ErrQueryFailed   = errors.New("query failed")
+	ErrUsernameTaken = errors.New("username already taken")
+	ErrEmailUsed     = errors.New("email already used")
 )
--- internal/db/user.go
+++ internal/db/user.go
@@ -2,6 +2,7 @@ package db
 
 import (
 	"tokenbase/internal/models"
+	"tokenbase/internal/utils"
 
 	"github.com/surrealdb/surrealdb.go"
 )
@@ -42,4 +43,62 @@ func ValidateUserCredentials(sdb *surrealdb.DB, email string, password string) (
 	}
 
 	return data.Result[0], nil
+}
+
+// Inserts a newly registered user's credentials into the DB
+//
+// Parameters:
+// - sdb: SurrealDB client
+// - username: User's username
+// - email: User's email
+// - password: User's password in plaintext
+// Returns:
+func RegisterUser(sdb *surrealdb.DB, username string, email string, password string) (string, error, error) { // return a token
+
+	const query = "SELECT username FROM users WHERE username = $username"
+	res_u, err := surrealdb.Query[[]models.DbUser](sdb, query, map[string]any{
+		"username": username,
+	})
+
+	if err != nil {
+		return "", err, nil
+	}
+
+	const query_2 = "SELECT email FROM users WHERE email = $email"
+	res_e, err := surrealdb.Query[[]models.DbUser](sdb, query, map[string]any{
+		"email": email,
+	})
+
+	if err != nil {
+		return "", err, nil
+	}
+
+	if len(*res_e) != 0 && len(*res_u) != 0 {
+		return "", ErrUsernameTaken, ErrEmailUsed
+	}
+
+	if len(*res_u) != 0 {
+		return "", ErrUsernameTaken, nil
+	}
+
+	if len(*res_e) != 0 {
+		return "", ErrEmailUsed, nil
+	}
+
+	user := map[string]any{
+		"username": username,
+		"email":    email,
+		"password": password,
+		"isAdmin":  false,
+	}
+
+	_, err = surrealdb.Insert[map[string]any](sdb, "users", user)
+
+	if err != nil {
+		return "", err, nil
+	}
+
+	token, err := utils.GenerateJwt(user)
+
+	return token
 }
